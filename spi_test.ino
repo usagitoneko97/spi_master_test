53f9d0af621a0d219bbb2281bdf6a32af2f85f0c
Ho Guo Xian <hogouxian@gmail.com>
2017-07-25 19:26:19 +0800

    successfully execute correct command whenever stm request

--- spi_test.ino
+++ spi_test.ino
@@ -1,15 +1,14 @@
 #include <SPI.h>
 uint8_t buffer1;
-uint8_t buffer2;
-int  serIn;
-byte bufferSerial[3] = {0xff};
 const uint8_t DUMMY = 0xff;
 const uint8_t NOP = 0xff;
-const uint8_t NEED_WIFI = 0x2;
+const uint8_t NEED_WIFI = 0x02;
 const uint8_t DONE_SENDING = 0x3;
-uint8_t* WifiSSID;
-uint8_t* WifiPW;
-uint8_t PWSSIDlength;
+const uint8_t NEED = 1;
+const uint8_t XNEED = 0;
+/*Mock wifiSSID*/ 
+uint8_t WifiSSID[8] = {0x89, 0x2d, 0x89, 0x78, 0x98, 0x54, 0x84, 0x28};
+uint8_t WifiPW[8] = {0x23, 0x18, 0x1d, 0xad, 0x03, 0x82, 0x92, 0x12};
 
 void setup (void)
 {
@@ -17,7 +16,6 @@ void setup (void)
   pinMode(15, OUTPUT);
   digitalWrite(15, HIGH); 
 
-  delay(1000);
   // Put SCK, MOSI, SS pins into output mode
   // also put SCK, MOSI into LOW state, and SS into HIGH state.
   // Then put SPI hardware into Master mode and turn SPI on
@@ -40,30 +38,35 @@ void loop (void)
   delay(100);
   }while(buffer1 != 0x85);
   */
-   if(Serial.available()) {
-     while (Serial.available()>0){
-        buffer = SPI.transfer(DUMMY);
+     SPI.transfer(NEED_WIFI);
+     buffer1 = SPI.transfer(DUMMY);
         
-        if(buffer == NEED_WIFI){
-          //getWifiSsid(WifiSSID, &PWSSIDlength);
-          SpiTransferArray(WifiSSID);
-          SPI.transfer(PWSSIDlength);
+     if(buffer1 == NEED){
+       //getWifiSsid(WifiSSID, &PWSSIDlength);
+        
+      /* SpiTransferArray(WifiSSID);
           //getWifiPW(WifiPW, , &PWSSIDlength);
-          SpiTransferrArray(WifiPw);
-          SPI.transfer(PWSSIDlength);
+       SpiTransferArray(WifiPW);*/
+       Serial.println("done");
+       delay(1000);
         }
-        else if (buffer == NOP){  //else?
-          
-        }
-        
-      }
-     }
-     
+     else if (buffer1 == XNEED){  //else?
+         Serial.println("NOP");
+       }
+     Serial.println(buffer1);
+     delay(10);
     //the serial buffer is over just go to the line (or pass your favorite stop char)               
     //Serial.println();
-  }
-  SpiTransferArray(uint8_t* data){
+}
+
+
+void SpiTransferArray(uint8_t* data){
     //TODO: code:
+    int i;
+    for(i = 0;i < 8;i++){
+      SPI.transfer(data[i]);
+      delay(10);
+    }
   }
   
   //SPI.transfer(e);
